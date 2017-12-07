030293f78c576e95c097eafd1f25d86527060cba
Judicael RAMANAMPISON <judicael.ramanampison@netapsys.fr>
2017-12-07 15:28:28 +0300

    debut class

---
+++ src/app/models/fam.ts
@@ -0,0 +1,12 @@
+import {Place} from './place';
+
+export class Fam {
+    private num:number;
+    private fam:String;
+    private symmary:String;
+    private event:String;
+    private date:Date;
+    private place:Place;
+    // private timeLong:Time;
+
+}
